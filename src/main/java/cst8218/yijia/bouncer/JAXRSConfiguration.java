389bb42993505275348a91bfea07622aaaabfc7c
Yijia Xu <yijiaxu.yjx@gmail.com>
2024-04-02 13:47:52 -0400

    Added lab4 AppUser classes

--- src/main/java/cst8218/yijia/bouncer/JAXRSConfiguration.java
+++ src/main/java/cst8218/yijia/bouncer/JAXRSConfiguration.java
@@ -4,10 +4,11 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * Configures JAX-RS for the application.
+ * This class implements data security, links to the database through annotations, and uses the browser's own methods to log in.
+ *
  * @author Juneau
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-    
+
 }
---
+++ src/main/java/cst8218/yijia/bouncer/entity/AppUser.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cst8218.yijia.bouncer.entity;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.security.enterprise.identitystore.PasswordHash;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Yijia
+ */
+@Entity
+@Table(name = "appuser")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "AppUser.findAll", query = "SELECT c FROM AppUser c"),
+    @NamedQuery(name = "AppUser.findByUserid", query = "SELECT c FROM AppUser c WHERE c.id = :id"),
+    @NamedQuery(name = "AppUser.findByPassword", query = "SELECT c FROM AppUser c WHERE c.password = :password"),
+    @NamedQuery(name = "AppUser.findByGroupname", query = "SELECT c FROM AppUser c WHERE c.groupname = :groupname")})
+public class AppUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    @Column(name = "ID")
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "GROUPNAME")
+    private String groupname;
+
+    public AppUser() {
+    }
+
+    public AppUser(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return "";
+    }
+
+    public void setPassword(String password) {
+        if ("".equals(password) && this.password != "") {
+            return;
+        }
+        // initialize a PasswordHash object which will generate password hashes
+        Instance<? extends PasswordHash> instance = CDI.current().select(Pbkdf2PasswordHash.class);
+        PasswordHash passwordHash = instance.get();
+        passwordHash.initialize(new HashMap<String,String>()); // todo: are the defaults good enough?
+        // now we can generate a password entry for a given password
+        String passwordEntry = password; //pretend the user has chosen a password mySecretPassword
+        passwordEntry = passwordHash.generate(passwordEntry.toCharArray());
+        //at this point, passwordEntry refers to a salted/hashed password entry corresponding to mySecretPassword
+        this.password = passwordEntry;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof AppUser)) {
+            return false;
+        }
+        AppUser other = (AppUser) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.lab4.AppUser[ id=" + id + " ]";
+    }
+
+}
